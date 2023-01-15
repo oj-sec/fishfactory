e3b00ef316892a98754bd1952f469f921a80508e
Oliver Smith <oliver.smith.mail@gmail.com>
2023-01-15 14:16:52 +1100

    add ssl fingerprint to recon

--- fishfactory_scrapy/spiders/spotter_spider.py
+++ fishfactory_scrapy/spiders/spotter_spider.py
@@ -1,8 +1,9 @@
 import base64
 import scrapy
 import socket
-from scrapy_splash import SplashRequest
+import ssl
 import hashlib
+from scrapy_splash import SplashRequest
 from urllib.parse import urlparse 
 
 class SpotterSpider(scrapy.Spider):
@@ -20,15 +21,38 @@ class SpotterSpider(scrapy.Spider):
         yield SplashRequest(url, self.screenshot, endpoint='render.json', args=splash_args)
                   
     def screenshot(self, response):
+        # Resolve DNS name to IP
         try:
             domain = urlparse(response.url).netloc
             ip = socket.gethostbyname(domain)
         except:
             ip = ''
 
+        # Take and procees screenshot
         imgdata = base64.b64decode(response.data['png'])
         imghash = hashlib.sha256(imgdata).hexdigest()
 
+        # Retrieve SSL certificate
+        ssl_fingerprint = ""
+        if response.url.startswith("https://"):
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.settimeout(3)
+
+            ctx = ssl.create_default_context()
+            phishing_hostname = urlparse(url).netloc
+            wrappedSocket = ctx.wrap_socket(sock, server_hostname=phishing_hostname)
+
+            try:
+                wrappedSocket.connect((phishing_hostname, 443))
+            except:
+                response = False
+            else:
+              der_cert = wrappedSocket.getpeercert(True)
+              thumb_sha1 = hashlib.sha1(der_cert).hexdigest()
+              ssl_fingerprint = thumb_sha1
+
+        # Retrieve HTTP title
         title = ""
         try:
             title = response.xpath('//title/text()').get()
@@ -45,6 +69,7 @@ class SpotterSpider(scrapy.Spider):
             spotter_record['phishingUrlTitle'] = title
             spotter_record['screenshotHash'] = imghash
             spotter_record['phishingIp'] = ip
+            spotter_record['sslFingerprint'] = ssl_fingerprint
             #spotter_record['screenshotData'] = response.data['png']
 
         return spotter_record
